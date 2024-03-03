0fb2d7da25613ad4421cc5a2335dcb1a12d448d9
hayatshin <hayat_shin@naver.com>
2024-03-04 00:44:10 +0900

    env

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:onldocc_admin/firebase_options.dart';
 import 'package:onldocc_admin/router.dart';
@@ -10,7 +9,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   try {
-    await dotenv.load(fileName: "env");
+    // await dotenv.load(fileName: "env");
     // await dotenv.load();
 
     WidgetsFlutterBinding.ensureInitialized();
@@ -19,12 +18,8 @@ void main() async {
       options: DefaultFirebaseOptions.currentPlatform,
     );
 
-    final supabaseUrl = dotenv.env["SUPABASE_URL"];
-    final supabaseAnonKey = dotenv.env["SUPABASE_ANONKEY"];
-
-    if (supabaseUrl == null || supabaseAnonKey == null) {
-      throw Exception("Supabase URL or Anon Key is not provided");
-    }
+    const supabaseUrl = String.fromEnvironment('SUPABASE_URL');
+    const supabaseAnonKey = String.fromEnvironment('SUPABASE_ANONKEY');
 
     await Supabase.initialize(
       url: supabaseUrl,
